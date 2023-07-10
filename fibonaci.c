c374279612188c1f9702765ce4d8044db10e5fab
Gabriel Silva <gabriellrezzendee@gmail.com>
2023-07-10 12:16:48 -0300

    Add files via upload

---
+++ fibonaci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+fibo(int n){
+    int a,b,c,contador,termo;
+
+    printf("Qual posição? ");
+    scanf("%d",&contador);
+
+    while(contador != termo){
+        termo
+    }
+    
+    
+}
+
+int main (void){
+    int p;
+    printf("Qual posição? ");
+    scanf("%d",&p);
+
+    fibo(p);
+}
+
