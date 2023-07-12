ee2183c1ee1c2aae7f03c07d467df40864091ced
Gabriel Silva <gabriellrezzendee@gmail.com>
2023-07-12 04:57:57 -0300

    Update fibonaci.c

--- fibonaci.c
+++ fibonaci.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 
-fibo(int n){
-    int a,b,c,contador,termo;
+fibo(int pos){
+        int a,b,c,p,termo;
+        a=0;
+        b=1;
 
-    printf("Qual posição? ");
-    scanf("%d",&contador);
+        
+        if (pos==1){
+                printf("0");
+        }else if (pos==2){
+                printf("0\n1");
+        }else{
+                printf("\n0");
+                for (int i=0; i<pos; i++){
+                        termo=a+b;
+                        
+                        printf("\n%d",termo);
+                        a=b;
+                        b=termo;
+                        
+                }
+        }
 
-    while(contador != termo){
-        termo
-    }
-    
     
 }
 
